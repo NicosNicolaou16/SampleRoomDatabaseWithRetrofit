dd1e991912a30b597449071bf3c612eb305aeddd
Nicos Nicolaou <nicosnicos16@gmail.com>
2025-09-06 23:52:11 +0300

    updates

---
+++ app/build.gradle.kts
@@ -0,0 +1,119 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-parcelize")
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
+    id("com.google.devtools.ksp")
+    id("androidx.navigation.safeargs.kotlin")
+}
+
+android {
+    compileSdk = 36
+    buildToolsVersion = "36.0.0"
+    namespace = "com.nick.sampleroomandretrofit"
+
+    defaultConfig {
+        applicationId = "com.nick.sampleroomandretrofit"
+        minSdk = 29
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0.0"
+        multiDexEnabled = true
+
+        buildFeatures {
+            dataBinding = true
+            viewBinding = true
+        }
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isShrinkResources = true
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+        debug {
+            isShrinkResources = true
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+    kotlin {
+        compilerOptions {
+            jvmTarget = JvmTarget.fromTarget("21")
+        }
+    }
+}
+
+val lifeCycleExtensionsVersion by extra("1.1.1")
+val lifeCycleAndLiveDataCompilerAndViewModelKTXVersion by extra("2.9.3")
+val roomVersion by extra("2.7.2")
+val glideVersion by extra("5.0.4")
+val navVersion by extra("2.9.3")
+val coroutineVersion by extra("1.10.2")
+val multidexVersion by extra("2.0.1")
+val materialDesignVersion by extra("1.13.0")
+val retrofitVersion by extra("3.0.0")
+val hiltVersion by extra("2.57.1")
+val hiltCompilerVersion by extra("1.2.0")
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    //architecture
+    implementation("androidx.core:core-ktx:1.17.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    //unit test
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
+    //live cycle and data
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifeCycleAndLiveDataCompilerAndViewModelKTXVersion")
+    //KTX - Live data
+    //noinspection LifecycleAnnotationProcessorWithJava8
+    kapt("androidx.lifecycle:lifecycle-compiler:$lifeCycleAndLiveDataCompilerAndViewModelKTXVersion")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifeCycleAndLiveDataCompilerAndViewModelKTXVersion")
+    //room database
+    implementation("androidx.room:room-runtime:$roomVersion")
+    ksp("androidx.room:room-compiler:$roomVersion")
+    implementation("androidx.room:room-ktx:$roomVersion")
+    //navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
+    implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
+    //coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutineVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutineVersion")
+    //materials
+    implementation("com.google.android.material:material:$materialDesignVersion")
+    //multidex
+    implementation("androidx.multidex:multidex:$multidexVersion")
+    //Glide - load the images
+    implementation("com.github.bumptech.glide:glide:$glideVersion")
+    ksp("com.github.bumptech.glide:compiler:$glideVersion")
+    // Gson
+    implementation("com.google.code.gson:gson:2.13.1")
+    //Retrofit request
+    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
+    //Hilt
+    implementation("com.google.dagger:hilt-android:$hiltVersion")
+    ksp("com.google.dagger:hilt-compiler:$hiltVersion")
+    ksp("androidx.hilt:hilt-compiler:$hiltCompilerVersion")
+
+}
