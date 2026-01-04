022584ae242cae6ecd6c4402a06dbb7b92d289a4
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-01-05 00:15:48 +0200

    update libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,7 +62,7 @@ android {
 
 val lifeCycleAndLiveDataCompilerAndViewModelKTXVersion by extra("2.10.0")
 val roomVersion by extra("2.7.2")
-val glideVersion by extra("5.0.4")
+val glideVersion by extra("5.0.5")
 val navVersion by extra("2.9.6")
 val coroutineVersion by extra("1.10.2")
 val multidexVersion by extra("2.0.1")
