995e2f8df760bdd221f3892aa74a8a19a2f9c052
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-03-14 00:14:26 +0200

    update Navigation version to 2.9.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:9.1.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.0")
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.9.6")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.9.7")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
